9b5a4eb3afed55809328af36cba75c94bcb4345f
Peletic <its.peletic@gmail.com>
2024-02-19 13:53:22 -0800

    use 8080 instead of 443

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express();
 app.get('/', (req, res, next) => {console.log("Getting home"); next()})
 app.get('/', app.use(express.static('./dist')))
-app.listen(443, (ev) => {
+app.listen(8080, (ev) => {
   console.log("Listening!")
 })
