e9f73a31ce21d943531e87018f548435b47bfeaf
Peletic <its.peletic@gmail.com>
2024-02-19 14:14:13 -0800

    add ssl to express

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ app.get('/', app.use(express.static('./dist')))
 app.listen(8080, (ev) => {
   console.log("Listening!")
 })
-let server = https.createServer({key: 'etc/nginx/private', cert: 'etc/nginx/origin'},app)
+let server = https.createServer({key: '/etc/nginx/private', cert: '/etc/nginx/origin'},app)
 
 server.listen(8080)
