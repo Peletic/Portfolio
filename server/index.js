01655cd25294bcc9d2688d80fb4aaa7f8b531dc2
Peletic <its.peletic@gmail.com>
2024-02-19 13:21:04 -0800

    use 443.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
+app.get('/', (req, res, next) => {console.log("Getting home"); next()})
 app.get('/', app.use(express.static('./dist')))
-app.listen(80, (ev) => {
+app.listen(443, (ev) => {
   console.log("Listening!")
 })
