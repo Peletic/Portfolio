173f5643088fb8558ac8955b7a00a0e4ff348c32
Peletic <its.peletic@gmail.com>
2024-02-19 14:14:57 -0800

    fs

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express();
 const https = require('https')
+const {readFileSync} = require("fs");
 app.get('/', (req, res, next) => {console.log("Getting home"); next()})
 app.get('/', app.use(express.static('./dist')))
 app.listen(8080, (ev) => {
   console.log("Listening!")
 })
-let server = https.createServer({key: '/etc/nginx/private', cert: '/etc/nginx/origin'},app)
+let server = https.createServer({key: readFileSync('/etc/nginx/private'), cert: fs.readFileSync('/etc/nginx/origin')},app)
 
 server.listen(8080)
