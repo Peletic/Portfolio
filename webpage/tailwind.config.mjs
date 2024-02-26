b07a7edc23917bf8296d807efe5a58d8e225b229
Peletic <its.peletic@gmail.com>
2024-02-25 17:59:14 -0800

    greeting card is still ugly but no one is here to give me feedback

--- webpage/tailwind.config.mjs
+++ webpage/tailwind.config.mjs
@@ -1,60 +1,116 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	daisyui: {
-		themes: [
-			{
-				dark: {
-					"primary": "#38385f",
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
 
-					"secondary": "#c23e7e",
+    extend: {
 
-					"accent": "#e0a177",
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": {transform: "rotate(215deg) translateX(0)", opacity: 1},
+          "70%": {opacity: 1},
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: 0,
+          },
+        },
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          "primary": "#77939f",
 
-					"neutral": "#2f2f49",
+          "secondary": "#c23e7e",
 
-					"base-100": "#620c24",
+          "accent": "#e0a177",
 
-					"info": "#3c807b",
+          "neutral": "#2f2f49",
 
-					"success": "#69a050",
+          "base-100": "#620c24",
 
-					"warning": "#e18b79",
+          "info": "#3c807b",
 
-					"error": "#6f0e1f",
-				},
-				postcard: {
-					"primary": "#ffd093",
+          "success": "#69a050",
 
-					"secondary": "#851539",
+          "warning": "#e18b79",
 
-					"accent": "#2d0746",
+          "error": "#6f0e1f",
+        },
+        postcard: {
+          "primary": "#ffd093",
 
-					"neutral": "#a5392f",
+          "secondary": "#851539",
 
-					"base-100": "#ff5e48",
+          "accent": "#2d0746",
 
-					"info": "#3c807b",
+          "neutral": "#a5392f",
 
-					"success": "#69a050",
+          "base-100": "#ff5e48",
 
-					"warning": "#e18b79",
+          "info": "#3c807b",
 
-					"error": "#6f0e1f",
-				}
-			}
-		], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		base: true, // applies background color and foreground color for root element by default
-		styled: true, // include daisyUI colors and design decisions for all components
-		utils: true, // adds responsive and modifier utility classes
-		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-		themeRoot: ":root", // The element that receives theme color CSS variables
+          "success": "#69a050",
 
-	},
-	plugins: [require("daisyui")],
+          "warning": "#e18b79",
+
+          "error": "#6f0e1f",
+        }
+      }
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+
+  },
+  plugins: [require("daisyui"), addVariablesForColors, function ({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        "bg-grid": (value) => ({
+          backgroundImage: `url("${svgToDataUri(
+            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+          )}")`,
+        }),
+        "bg-grid-small": (value) => ({
+          backgroundImage: `url("${svgToDataUri(
+            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+          )}")`,
+        }),
+        "bg-dot": (value) => ({
+          backgroundImage: `url("${svgToDataUri(
+            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+          )}")`,
+        }),
+      },
+      { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+    );
+  },],
+}
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({addBase, theme}) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
 }
